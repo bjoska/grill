81df2bdf6b0dec2a72983d7963ad635a0fe93cc5
Björn Skarner <bjorn@def-end.se>
2013-07-06 03:41:16 +0200

    Changes made to the CommentFormView, CommentFormTemplate and CommentController.
    This should enable the ability of saving a comment.

--- app/assets/javascripts/controllers/comment.js
+++ app/assets/javascripts/controllers/comment.js
@@ -1,5 +1,10 @@
 App.CommentController = Em.ObjectController.extend({
   createNew: function() {
-    // ??
+    var body = this.get('content');
+    var comment = App.Comment.createRecord({
+      body: body
+    });
+    this.set('content', 'Comment saved!');
+    comment.save();
   }
 });
--- app/assets/javascripts/views/comment_form.js
+++ app/assets/javascripts/views/comment_form.js
@@ -1,9 +1,5 @@
 App.CommentFormView = Ember.View.extend({
   tagName: "form"
-, templateName: "comments/form"
-, controller: App.CommentController.create({ content: App.Comment.createRecord()})
-, submit: function(e) {
-    e.preventDefault();
-    // how to instantiate controller
-  }
+  , templateName: "comments/form"
+  , controller: App.CommentController.create()
 });
